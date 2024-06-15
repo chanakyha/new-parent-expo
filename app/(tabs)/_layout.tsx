168d00510175875e33600a6af7415ca216be2e3e
chanakyha <vetrichanakyha2003@gmail.com>
2024-06-15 14:11:46 +0530

    added a boiler page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,17 @@ import { Tabs } from "expo-router";
 import React from "react";
 
 const TabsLayout = () => {
-  return <Tabs></Tabs>;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarLabel: "Home",
+        }}
+      />
+    </Tabs>
+  );
 };
 
 export default TabsLayout;
---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const IndexPae = () => {
+  return (
+    <View>
+      <Text>IndexPage</Text>
+    </View>
+  );
+};
+
+export default IndexPae;
+
+const styles = StyleSheet.create({});
